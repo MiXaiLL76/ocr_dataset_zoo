1c332b25c7e0a7409c87bfd4597f3bfc88a43f86
MiXaiLL76 <mike.milos@yandex.ru>
2025-01-19 09:42:08 +0300

    add IIIT5K

---
+++ IIIT5K/README.md
@@ -0,0 +1,60 @@
+# IIIT5K
+
+## META
+<https://github.com/open-mmlab/mmocr/blob/main/dataset_zoo/iiit5k/metafile.yml>
+
+```yaml
+Name: 'IIIT5K'
+Paper:
+  Title: Scene Text Recognition using Higher Order Language Priors
+  URL: http://cvit.iiit.ac.in/projects/SceneTextUnderstanding/Home/mishraBMVC12.pdf
+  Venue: BMVC
+  Year: '2012'
+  BibTeX: '@InProceedings{MishraBMVC12,
+  author    = "Mishra, A. and Alahari, K. and Jawahar, C.~V.",
+  title     = "Scene Text Recognition using Higher Order Language Priors",
+  booktitle = "BMVC",
+  year      = "2012"}'
+Data:
+  Website: http://cvit.iiit.ac.in/projects/SceneTextUnderstanding/IIIT5K.html
+  Language:
+    - English
+  Scene:
+    - Natural Scene
+  Granularity:
+    - Word
+  Tasks:
+    - textrecog
+  License:
+    Type: N/A
+    Link: N/A
+  Format: .txt
+```
+
+## DOWNLOAD
+
+```bash
+
+wget --no-check-certificate -O IIIT5K.tar.gz http://cvit.iiit.ac.in/projects/SceneTextUnderstanding/IIIT5K-Word_V3.0.tar.gz
+wget --no-check-certificate https://download.openmmlab.com/mmocr/data/mixture/IIIT5K/train_label.txt
+wget --no-check-certificate https://download.openmmlab.com/mmocr/data/mixture/IIIT5K/test_label.txt
+
+```
+
+## CONVERT 
+
+```bash
+python3 convert_to_general_format.py --labels train_label.txt --images_dir ./IIIT5K/train
+python3 convert_to_general_format.py --labels test_label.txt --images_dir ./IIIT5K/test
+
+```
+
+## CONVERT TO HUGGINGFACE FORMAT
+
+<https://huggingface.co/datasets/MiXaiLL76/IIIT5K_OCR>
+
+## READY DATASETS
+
+| TRAIN                                            | TEST                                           |
+| ------------------------------------------------ | ---------------------------------------------- |
+| ![train_image](.examples/You_1009_2.png "Train Image") | ![test_image](.examples/PRIVATE_1002_1.png "Test Image") |
---
+++ IIIT5K/convert_to_general_format.py
@@ -0,0 +1,106 @@
+import os
+import cv2
+import json
+import tqdm
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import defaultdict
+
+def process_icdar(labels, images_dir):
+    dataframe = []
+    with open(labels, encoding='utf-8-sig') as fd:
+        for line in fd.readlines():
+            dataframe.append({
+                "file" : line[:line.index(" ")].strip(),
+                "text" : line[line.index(" ")+1:].strip().strip(),
+            })
+
+    for item in tqdm.tqdm(dataframe):
+        text = item["text"]
+        image_name = item["file"]
+        image_file_name = os.path.join(images_dir, image_name)
+        img = cv2.imread(image_file_name, cv2.IMREAD_UNCHANGED)
+        x1, y1 = 0, 0
+        y2, x2 = (np.array(img.shape[:2]) - [1, 1]).tolist()
+
+        row = {
+            "image_name": image_file_name,
+            "text": text,
+            "bbox": [x1, y1, x2 - x1, y2 - y1],
+        }
+
+        yield row
+
+
+def display_data(row):
+    data = cv2.imread(row["image_name"])
+
+    fig, ax = plt.subplots()
+
+    title = []
+    title.append(f"{row['text']}")
+    cv2.rectangle(
+        data,
+        (int(row["bbox"][0]), int(row["bbox"][1])),
+        (
+            int(row["bbox"][0]) + int(row["bbox"][2]),
+            int(row["bbox"][1]) + int(row["bbox"][3]),
+        ),
+        (0, 255, 0),
+        2,
+    )
+    title_kargs = {}
+    ax.set_title(" | ".join(title), **title_kargs)
+    ax.imshow(data)
+    fig.canvas.draw()  # Draw the canvas, cache the renderer
+
+    # Convert the canvas to a raw RGB buffer
+    buf = fig.canvas.buffer_rgba()
+    ncols, nrows = fig.canvas.get_width_height()
+    image = np.frombuffer(buf, dtype=np.uint8).reshape(nrows, ncols, 4)
+    image = cv2.cvtColor(image, cv2.COLOR_RGBA2RGB)
+
+    key = "_".join(title).replace(" ", "_")
+    cv2.imwrite(f".examples/{key}_{os.path.basename(row['image_name'])}", image)
+
+    return image
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--labels", type=str, default="./train_label.txt")
+    parser.add_argument(
+        "--images_dir", type=str, default="./IIIT5K/train"
+    )
+    parser.add_argument("--display_first", type=bool, default=True)
+
+    args = parser.parse_args()
+
+    os.makedirs(args.images_dir, exist_ok=True)
+
+    out_dataset = defaultdict(list)
+
+    for row in process_icdar(args.labels, args.images_dir):
+        if args.display_first:
+            display_data(row)
+            args.display_first = False
+
+        out_dataset[row["image_name"]].append(
+            {"text": row["text"], "bbox": row["bbox"]}
+        )
+
+    with open(os.path.join(args.images_dir, "ann_file.jsonl"), "w") as fd:
+        fd.write("")
+
+    for image_file_name, texts in out_dataset.items():
+        with open(os.path.join(args.images_dir, "ann_file.jsonl"), "a") as fd:
+            fd.write(
+                json.dumps(
+                    {
+                        "filename": os.path.basename(image_file_name),
+                        "annotations": texts,
+                    }
+                )
+            )
+            fd.write("\n")
